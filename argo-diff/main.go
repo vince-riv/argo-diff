0690d2ca8e900cd6d4f8c8ad1a14c35be12bb308
Vincent Rivellino <github@vince-riv.io>
2023-11-28 18:44:05 -0500

    Process event data in goroutines

--- argo-diff/main.go
+++ argo-diff/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"sync"
+	"syscall"
 	"time"
 
 	"argo-diff/argocd"
@@ -20,82 +24,23 @@ import (
 var (
 	githubWebhookSecret string
 	devMode             bool
+	wg                  sync.WaitGroup
 )
 
 const gitRevTxt = "git-rev.txt"
 
 const sigHeaderName = "X-Hub-Signature-256"
 
-func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	payload, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Error().Err(err).Msg("Error reading request body")
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	if devMode {
-		log.Info().Msg("Running in dev mode - skipping signature validation")
-	} else {
-		signature := r.Header.Get(sigHeaderName)
-		if !webhook.VerifySignature(payload, signature, githubWebhookSecret) {
-			http.Error(w, "Invalid signature", http.StatusUnauthorized)
-			return
-		}
-	}
-
-	event := r.Header.Get("X-GitHub-Event")
-	eventInfo := webhook.NewEventInfo()
-	switch event {
-	case "ping":
-		log.Info().Str("method", r.Method).Str("url", r.URL.String()).Msg("ping event received")
-		_, err := io.WriteString(w, "ping event processed\n")
-		if err != nil {
-			log.Error().Err(err).Msg("io.WriteString() failed")
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
-		return // we're done with a ping event
-	case "pull_request":
-		eventInfo, err = webhook.ProcessPullRequest(payload)
-		if err != nil {
-			http.Error(w, "Could not process pull request event data", http.StatusInternalServerError)
-			return
-		}
-	case "push":
-		eventInfo, err = webhook.ProcessPush(payload)
-		if err != nil {
-			http.Error(w, "Could not process push event data", http.StatusInternalServerError)
-			return
-		}
-	default:
-		log.Info().Str("method", r.Method).Str("url", r.URL.String()).Msgf("Ignoring X-GitHub-Event %s", event)
-		_, err := io.WriteString(w, "event ignored\n")
-		if err != nil {
-			log.Error().Err(err).Msg("io.WriteString() failed")
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
-		return // we're done with an event we don't know about
-	}
-	if eventInfo.Ignore {
-		log.Info().Msgf("Ignoring %s event. Event Info: %v", event, eventInfo)
-		_, err := io.WriteString(w, fmt.Sprintf("%s event ignored\n%v\n", event, eventInfo))
-		if err != nil {
-			log.Error().Err(err).Msg("io.WriteString() failed")
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
-		return // we're done with a PR/PUSH event we don't care about
-	}
+func processEvent(eventInfo webhook.EventInfo) {
+	defer wg.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
 	isPr := eventInfo.PrNum > 0
-	github.Status(r.Context(), isPr, github.StatusPending, "", eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
+	github.Status(ctx, isPr, github.StatusPending, "", eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
 	appManifests, err := argocd.GetApplicationManifests(eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.RepoDefaultRef, eventInfo.Sha, eventInfo.ChangeRef)
 	if err != nil {
-		github.Status(r.Context(), isPr, github.StatusError, err.Error(), eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
+		github.Status(ctx, isPr, github.StatusError, err.Error(), eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
 		log.Error().Err(err).Msg("argocd.GetApplicationManifests() failed")
-		_, err := io.WriteString(w, "event accepted; processing failed\n")
-		if err != nil {
-			log.Error().Err(err).Msg("io.WriteString() failed")
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
 		return // we're done due to a processing error
 
 	}
@@ -157,13 +102,80 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		newStatus = github.StatusSuccess
 		statusDescription = fmt.Sprintf("%s - no errors", changeCountStr)
 	}
-	github.Status(r.Context(), isPr, newStatus, statusDescription, eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
+	github.Status(ctx, isPr, newStatus, statusDescription, eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.Sha, devMode)
 
 	if eventInfo.PrNum > 0 {
-		_, err = github.Comment(r.Context(), eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.PrNum, markdownStart+"\n\n"+markdown)
-		if err != nil {
-			_, _ = io.WriteString(w, "event accepted; github comment failed\n")
+		_, _ = github.Comment(ctx, eventInfo.RepoOwner, eventInfo.RepoName, eventInfo.PrNum, markdownStart+"\n\n"+markdown)
+		return
+	}
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	payload, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("Error reading request body")
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	if devMode {
+		log.Info().Msg("Running in dev mode - skipping signature validation")
+	} else {
+		signature := r.Header.Get(sigHeaderName)
+		if !webhook.VerifySignature(payload, signature, githubWebhookSecret) {
+			http.Error(w, "Invalid signature", http.StatusUnauthorized)
+			return
 		}
+	}
+
+	event := r.Header.Get("X-GitHub-Event")
+	eventInfo := webhook.NewEventInfo()
+	switch event {
+	case "ping":
+		log.Info().Str("method", r.Method).Str("url", r.URL.String()).Msg("ping event received")
+		_, err := io.WriteString(w, "ping event processed\n")
+		if err != nil {
+			log.Error().Err(err).Msg("io.WriteString() failed")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return // we're done with a ping event
+	case "pull_request":
+		eventInfo, err = webhook.ProcessPullRequest(payload)
+		if err != nil {
+			http.Error(w, "Could not process pull request event data", http.StatusInternalServerError)
+			return
+		}
+	case "push":
+		eventInfo, err = webhook.ProcessPush(payload)
+		if err != nil {
+			http.Error(w, "Could not process push event data", http.StatusInternalServerError)
+			return
+		}
+	default:
+		log.Info().Str("method", r.Method).Str("url", r.URL.String()).Msgf("Ignoring X-GitHub-Event %s", event)
+		_, err := io.WriteString(w, "event ignored\n")
+		if err != nil {
+			log.Error().Err(err).Msg("io.WriteString() failed")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return // we're done with an event we don't know about
+	}
+	if eventInfo.Ignore {
+		log.Info().Msgf("Ignoring %s event. Event Info: %v", event, eventInfo)
+		_, err := io.WriteString(w, fmt.Sprintf("%s event ignored\n%v\n", event, eventInfo))
+		if err != nil {
+			log.Error().Err(err).Msg("io.WriteString() failed")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return // we're done with a PR/PUSH event we don't care about
+	}
+	// call process event in a new gorouting and send a 200 OK back to Github
+	wg.Add(1)
+	go processEvent(eventInfo)
+	_, err = io.WriteString(w, "event accepted for processing\n")
+	if err != nil {
+		log.Error().Err(err).Msg("io.WriteString() failed")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
@@ -243,9 +255,31 @@ func init() {
 
 func main() {
 	log.Info().Msg("Setting up listener on port 8080")
+	srv := &http.Server{Addr: ":8080"}
 	http.HandleFunc("/webhook", handleWebhook)
 	http.HandleFunc("/webhook_log", printWebHook)
 	http.HandleFunc("/healthz", healthZ)
-	http.HandleFunc("/dev", devHandler)
-	log.Error().Err(http.ListenAndServe(":8080", nil)).Msg("")
+	if devMode {
+		http.HandleFunc("/dev", devHandler)
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("http.ListenAndServe(':8080', nil) failed")
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	<-stop // block until TERM or INT is received
+	log.Info().Msg("Shutting down...")
+
+	// Shut down the server with a context (for 30s timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Warn().Err(err).Msg("Server forced to shutdown")
+	}
+	// Wait for all goroutines to finish
+	wg.Wait()
+	log.Info().Msg("Server gracefully stopped")
 }
