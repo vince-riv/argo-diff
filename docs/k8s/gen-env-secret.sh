0b08d453fa4d8389e17b39748ea69a9eb473a423
Vincent Rivellino <github@vince-riv.io>
2024-01-31 08:27:27 -0500

    chore: README & docs update

--- docs/k8s/gen-env-secret.sh
+++ docs/k8s/gen-env-secret.sh
@@ -4,12 +4,26 @@ ns=argocd
 
 cd "$(dirname "$0")"
 
-read -p 'Github Webhook Secret: ' webhook_secret
-read -p 'Github API Token: ' api_token
 read -p 'ArgoCD Auth Token: ' argo_token
+read -p 'Github Webhook Secret: ' webhook_secret
+read -p 'Github Personal Access Token: ' api_token
+read -p 'Github App Private Key .pem file: ' app_private_key
+
+secret_args=()
+
+if [[ -n $argo_token ]]; then
+    secret_args+=("--from-literal=ARGOCD_AUTH_TOKEN=${argo_token}")
+fi
+if [[ -n $webhook_secret ]]; then
+    secret_args+=("--from-literal=GITHUB_PERSONAL_ACCESS_TOKEN=${api_token}")
+fi
+if [[ -n $api_token ]]; then
+    secret_args+=("--from-literal=GITHUB_WEBHOOK_SECRET=${webhook_secret}")
+fi
+if [[ -n $app_private_key ]]; then
+    secret_args+=("--from-file=GITHUB_APP_PRIVATE_KEY=${app_private_key}")
+fi
 
 kubectl -n $ns create secret generic argo-diff-env \
-    "--from-literal=GITHUB_WEBHOOK_SECRET=${webhook_secret}" \
-    "--from-literal=GITHUB_PERSONAL_ACCESS_TOKEN=${api_token}" \
-    "--from-literal=ARGOCD_AUTH_TOKEN=${argo_token}" \
-    --dry-run=client -o yaml | kubeseal -n $ns -o yaml > env-secret.yaml
+    ${secret_args[@]} \
+    --dry-run=client -o yaml | kubeseal -n $ns -o yaml --merge-into env-secret.yaml
