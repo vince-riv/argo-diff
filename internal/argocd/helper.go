9ce115f5fddc9b18853d8ca067b38c7574f671c6
Hans Knecht <hans@anomalo.com>
2025-04-06 13:52:04 +0000

    chore: logs

--- internal/argocd/helper.go
+++ internal/argocd/helper.go
@@ -251,7 +251,7 @@ func gitRepoMatch(repoUrl, repoOwner, repoName string) bool {
 	ghMatchExtension := fmt.Sprintf("github.com/%s/%s.git", repoOwner, repoName)
 	ghMatchClean := fmt.Sprintf("github.com/%s/%s", repoOwner, repoName)
 	ghMatchColon := fmt.Sprintf("github.com:%s/%s", repoOwner, repoName)
-	log.Debug().Msgf("gitRepoMatch() - matching candidates against '%s' and '%s'", ghMatchExtension, ghMatchClean)
+	log.Debug().Msgf("gitRepoMatch() - matching candidates against '%s', '%s' and '%s'", ghMatchExtension, ghMatchClean, ghMatchColon)
 	if !strings.HasSuffix(repoUrl, ghMatchExtension) && !strings.HasSuffix(repoUrl, ghMatchClean) && !strings.HasSuffix(repoUrl, ghMatchColon) {
 		return false
 	}
