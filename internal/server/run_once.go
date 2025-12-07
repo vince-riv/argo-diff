5c21d978977038ec87bd8d417cf533700f0c6c4c
Vincent Rivellino <github@vince-riv.io>
2025-12-06 20:57:11 -0500

    feat: additional debug logs in run_once.go

--- internal/server/run_once.go
+++ internal/server/run_once.go
@@ -14,6 +14,72 @@ import (
 	"github.com/vince-riv/argo-diff/internal/webhook"
 )
 
+// redactEnvValue returns a redacted version of an environment variable value.
+// For sensitive values, it shows only the first 3 characters followed by "***".
+// Returns "<not set>" if the variable is not set.
+func redactEnvValue(key string, isSensitive bool) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return "<not set>"
+	}
+	if !isSensitive {
+		return value
+	}
+	if len(value) <= 3 {
+		return "***"
+	}
+	return value[:3] + "***"
+}
+
+// logEnvironmentVariables logs all relevant environment variables for debugging,
+// redacting sensitive values to show only the first 3 characters.
+func logEnvironmentVariables() {
+	log.Debug().Msg("=== Environment Variables ===")
+
+	// Sensitive variables - show only first 3 chars
+	sensitiveVars := []string{
+		"ARGOCD_AUTH_TOKEN",
+		"GITHUB_WEBHOOK_SECRET",
+		"GITHUB_PERSONAL_ACCESS_TOKEN",
+		"GITHUB_TOKEN",
+		"GITHUB_APP_PRIVATE_KEY",
+	}
+	for _, key := range sensitiveVars {
+		log.Debug().Str(key, redactEnvValue(key, true)).Msg("")
+	}
+
+	// Non-sensitive variables - show full value
+	nonSensitiveVars := []string{
+		"ARGOCD_SERVER_ADDR",
+		"ARGOCD_UI_BASE_URL",
+		"ARGOCD_SERVER_INSECURE",
+		"ARGOCD_SERVER_PLAINTEXT",
+		"ARGOCD_GRPC_WEB",
+		"ARGOCD_GRPC_WEB_ROOT_PATH",
+		"ARGOCD_CLI_CMD_NAME",
+		"GITHUB_APP_ID",
+		"GITHUB_APP_INSTALLATION_ID",
+		"APP_ENV",
+		"LOG_LEVEL",
+		"GITHUB_ACTIONS",
+		"GITHUB_EVENT_NAME",
+		"GITHUB_REF",
+		"GITHUB_REPOSITORY",
+		"REPO_DEFAULT_REF",
+		"GITHUB_HEAD_REF",
+		"GITHUB_BASE_REF",
+		"ARGO_DIFF_CONTEXT_STR",
+		"ARGO_DIFF_CI",
+		"ARGO_DIFF_COMMENT_PREAMBLE",
+		"COMMENT_LINE_MAX_CHARS",
+	}
+	for _, key := range nonSensitiveVars {
+		log.Debug().Str(key, redactEnvValue(key, false)).Msg("")
+	}
+
+	log.Debug().Msg("=== End Environment Variables ===")
+}
+
 func eventInfoFromEnv() (*webhook.EventInfo, error) {
 	if ghEvent := os.Getenv("GITHUB_EVENT_NAME"); ghEvent != "pull_request" {
 		return nil, fmt.Errorf("unexpected value for GITHUB_EVENT_NAME: %s (expecting pull_request)", ghEvent)
@@ -64,6 +130,7 @@ func eventInfoFromFile(filePath string) (*webhook.EventInfo, error) {
 
 func ProcessFileEvent(filePath string, devMode bool) error {
 	log.Debug().Msgf("processFileEvent('%s')", filePath)
+	logEnvironmentVariables()
 	evtp, err := eventInfoFromFile(filePath)
 	if err != nil {
 		log.Error().Err(err).Msgf("Failed to read event from %s", filePath)
@@ -80,6 +147,7 @@ func ProcessFileEvent(filePath string, devMode bool) error {
 
 func ProcessGithubAction() error {
 	log.Debug().Msg("ProcessGithubAction()")
+	logEnvironmentVariables()
 	evtp, err := eventInfoFromEnv()
 	if err != nil {
 		return err
